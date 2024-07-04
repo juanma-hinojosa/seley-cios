7bf66c6433eeef981b822248066c0e555152dba0
AyrtonDev22 <ayrtonddev@gmail.com>
2024-07-03 23:32:52 -0300

    Almost final footer

--- src/componets/FooterComponent.jsx
+++ src/componets/FooterComponent.jsx
@@ -1,44 +1,35 @@
 import '../css/footer-component.css'
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
 
 function FooterComponent() {
     return(
-        <footer className="footer-wrapper poppins-regular">
-            <div className="footer-section-one">
-                <div className="footer-logo-container">
-                    <h1>LOGO</h1>
-                    <img src="" alt="" />
-
+        <footer className="pie-pagina">
+            <div class="grupo-1">
+                <div class="box">
+                    <figure>
+                        <a href="#">
+                            <h1>LOGO</h1>
+                            <img src="" alt="" />
+                        </a>
+                    </figure>
                 </div>
-                <div className="footer-icons">
-                <BsTwitter />
-                <SiLinkedin />
-                <BsYoutube />
-                <FaFacebookF />
+                <div class="box">
+                    <p>1138393736</p>
+                    <p>hello@dental.com</p>
+                    <p>press@dental.com</p>
+                    <p>contact@dental.com</p>
                 </div>
-            </div>
-            <div className="footer-section-two">
-                <div className="footer-section-columns">
-                <span>Quality</span>
-                <span>Help</span>
-                <span>Share</span>
-                <span>Carrers</span>
-                <span>Testimonials</span>
-                <span>Work</span>
+                <div class="box">
+                    <h2>SIGUENOS</h2>
+                    <div class="red-social">
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-youtube"></a>
+                    </div>
                 </div>
-                <div className="footer-section-columns">
-                    <span>1138393736</span>
-                    <span>hello@dental.com</span>
-                    <span>press@dental.com</span>
-                    <span>contact@dental.com</span>
-                </div>
-                <div className="footer-section-columns">
-                    <span>Terms & Conditions</span>
-                    <span>Privacy Policy</span>
-                </div>
+            </div>            
+            <div className="grupo-2">
+                <small>&copy; 2024 <b>Seley C.I.O.S.</b> - Todos los Derechos Reservados.</small>
             </div>
         </footer>
     )
